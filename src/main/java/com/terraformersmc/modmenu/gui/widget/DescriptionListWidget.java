7be2e44964287d58a542e762a052b59c8e4659da
Lilly Rose Berner <lilly@lostluma.net>
2024-04-22 21:53:31 +0200

    Group mod credits by role instead of bunching them together (#706)
    * Display credits by role instead of bunching them together
    
    * Add newlines between credit groups, allow translating role names
    
    * Fix minor code style issues
    
    * Improve translation key generation and fallback role names
    
    * Sort contributors within roles
    
    * Fix Fabric authors being credited wrong
    
    - Group mod credits together by role

--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -19,9 +19,12 @@ import net.minecraft.client.render.*;
 import net.minecraft.resource.language.I18n;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
 
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
@@ -207,7 +210,8 @@ public class DescriptionListWidget extends EntryListWidget {
 					if ("minecraft".equals(mod.getId())) {
 						this.entries.add(emptyEntry);
 					} else if (!"java".equals(mod.getId())) {
-						List<String> credits = mod.getCredits();
+						SortedMap<String, SortedSet<String>> credits = mod.getCredits();
+
 						if (!credits.isEmpty()) {
 							this.entries.add(emptyEntry);
 
@@ -215,11 +219,30 @@ public class DescriptionListWidget extends EntryListWidget {
 								this.entries.add(new DescriptionEntry((String) line));
 							}
 
-							for (String credit : credits) {
+							Iterator<Map.Entry<String, SortedSet<String>>> iterator = credits.entrySet().iterator();
+
+							while (iterator.hasNext()) {
 								int indent = 8;
-								for (Object line : textRenderer.split(credit, wrapWidth - 16)) {
+
+								Map.Entry<String, SortedSet<String>> role = iterator.next();
+								String roleName = role.getKey();
+
+								for (Object line : textRenderer.split(this.creditsRoleText(roleName), wrapWidth - 16)) {
 									this.entries.add(new DescriptionEntry((String) line, indent));
 									indent = 16;
+								}
+
+								for (String contributor : role.getValue()) {
+									indent = 16;
+
+									for (Object line : textRenderer.split(contributor, wrapWidth - 24)) {
+										this.entries.add(new DescriptionEntry((String) line, indent));
+										indent = 24;
+									}
+								}
+
+								if (iterator.hasNext()) {
+									this.entries.add(emptyEntry);
 								}
 							}
 						}
@@ -404,6 +427,14 @@ public class DescriptionListWidget extends EntryListWidget {
 		}
 
 		minecraft.openScreen(this.parent);
+	}
+
+	private String creditsRoleText(String roleName) {
+		// Replace spaces and dashes in role names with underscores if they exist
+		// Notably Quilted Fabric API does this with FabricMC as "Upstream Owner"
+		String translationKey = roleName.replaceAll("[\\s-]", "_");
+
+		return I18n.translate("modmenu.credits.role." + translationKey) + ":";
 	}
 
 	protected class DescriptionEntry implements EntryListWidget.Entry {
